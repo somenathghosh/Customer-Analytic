630e904f8130e02173df53381758973dd78ce282
SOMENATH GHOSH <somenathghosh@somenaths-air.attlocal.net>
2015-01-07 12:39:08 -0500

    init

--- app.js
+++ app.js
@@ -10,7 +10,89 @@ var users = require('./routes/users');
 var db = require('node-mysql');
 var mongoose = require('mongoose');
 
-var r = require('./util/Results.js');
+/*var r = require('./util/Results.js');*/
+
+
+
+
+////
+
+
+function Results  () {
+    
+}
+
+
+
+Results.prototype.addPersons = function(persons) {
+
+    this.persons = persons;
+    return this;
+};
+
+
+Results.prototype.addKeywords = function(keywords) {
+
+    //console.log(keywords);
+    this.keywords = keywords;
+    //console.log(this);
+    return this;
+};
+
+Results.prototype.getKeyWords = function(first_argument) {
+    return this.keywords;
+};
+
+Results.prototype.searchScore = function(searchString) {
+    var results = [];
+    //console.log(searchString);
+    this.keywords.forEach(function function_name (element, index, array) {
+        //console.log(element);
+        if( searchString.search(element) > 0) {
+
+            results.push(element);
+        }
+
+    })
+
+    return results;
+
+};
+
+
+Results.prototype.run = function(results, callback) {
+
+    //console.log(results);
+    
+
+    score = [];
+
+    
+
+    var that = this;
+    results.forEach(function function_name (element, index, array) {
+        var rank = that.searchScore(element.description.toUpperCase());
+        if(rank.length !== 0) {
+            var temp = {};
+            temp.name = element.name;
+            temp.date = new Date(element.date);
+            //temp.rank = that.searchScore(element.description.toUpperCase());
+            //console.log(temp);
+            temp.rank = rank
+            score.push(temp);
+
+        }
+    });
+
+    callback(score);
+        
+
+
+
+};
+
+var r = new Results();
+//////
 
 var DB = db.DB;
 
